1eaf08a6f0a580273ac7c7b4cd2ba2f7a16068b6
abdullaa mac <abdullaa.taha.at@gmail.com>
2022-08-26 12:58:35 +0300

    test

--- src/App.tsx
+++ src/App.tsx
@@ -8,41 +8,40 @@ import { Main, Incubator, AboutUs } from '@components/pages';
  @return {ReactElement} the main app 
 */
 function App() {
-    const dim = useWindowDimensions();
-    return (
-        <Router basename='/game-hub'>
-            {dim.width < 800 ? <BurgerMenu /> : <Header />}
-            <Routes>
-                <Route
-                    path='/'
-                    element={
-                        <>
-                            <Main />
-                        </>
-                    }
-                />
-
-                <Route
-                    path='/incubator'
-                    element={
-                        <>
-                            <Incubator />
-                        </>
-                    }
-                />
-
-                <Route
-                    path='/about-us'
-                    element={
-                        <>
-                            <AboutUs />
-                        </>
-                    }
-                />
-            </Routes>
-            <Footer />
-        </Router>
-    );
+    return <h1>efe</h1>;
+    // const dim = useWindowDimensions();
+    // return (
+    //     <Router basename='/game-hub'>
+    //         {dim.width < 800 ? <BurgerMenu /> : <Header />}
+    //         <Routes>
+    //             <Route
+    //                 path='/'
+    //                 element={
+    //                     <>
+    //                         <Main />
+    //                     </>
+    //                 }
+    //             />
+    //             <Route
+    //                 path='/incubator'
+    //                 element={
+    //                     <>
+    //                         <Incubator />
+    //                     </>
+    //                 }
+    //             />
+    //             <Route
+    //                 path='/about-us'
+    //                 element={
+    //                     <>
+    //                         <AboutUs />
+    //                     </>
+    //                 }
+    //             />
+    //         </Routes>
+    //         <Footer />
+    //     </Router>
+    // );
 }
 
 export default App;
